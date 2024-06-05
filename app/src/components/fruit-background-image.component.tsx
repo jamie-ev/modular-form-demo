cb22ff6e504cf7d901b7fadf576c230017a189d2
Jamie Ev. <jamie.osprey@gmail.com>
2024-06-04 17:05:28 -0700

    Add expandable question headers, styling

---
+++ app/src/components/fruit-background-image.component.tsx
@@ -0,0 +1,24 @@
+export const FruitBackgroundImage = () => (
+  <>
+    <div style={{
+        backgroundImage: 'url("./../../public/fruit-background.png")',
+        width: "100vw",
+        height: "100vh",
+        backgroundRepeat: "repeat",
+        backgroundSize: "contain",
+        position: "fixed",
+        top: "0",
+        left: "0",
+        zIndex: "-1"
+    }}/>
+    <div style={{
+        backgroundColor: 'rgba(245,220,240,0.45)',
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        top: "0",
+        left: "0",
+        zIndex: "-1"
+    }}/>
+  </>
+)
